28e93296d01c0b860227d822dac87ed3d23ac59d
arshnoor1411 <arshnoor@infocusp.com>
2024-05-29 15:59:48 +0530

    enhance: annotation line chart with multiple line chart colors added

--- line_chart.py
+++ line_chart.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
  
-
+# Line Chart
 x = np.array([1, 2, 3, 4])
 y = x*2
  
@@ -9,4 +9,39 @@ plt.plot(x, y)
 plt.xlabel("X-axis")
 plt.ylabel("Y-axis")
 plt.title("Any suitable title") 
+plt.show()
+ 
+# Annotation Line Chart
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+ 
+plt.figure(figsize=(8, 6))
+plt.plot(x, y, marker='o', linestyle='-')
+ 
+for i, (xi, yi) in enumerate(zip(x, y)):
+    plt.annotate(f'({xi}, {yi})', (xi, yi), textcoords="offset points", xytext=(0, 10), ha='center')
+ 
+plt.title('Line Chart with Annotations')
+plt.xlabel('X-axis Label')
+plt.ylabel('Y-axis Label')
+ 
+plt.grid(True)
+ 
+plt.show()
+
+# Multiple Line Chart
+x = np.array([1, 2, 3, 4])
+y = x*2
+ 
+plt.plot(x, y)
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("Any suitable title")
+plt.show() 
+
+plt.figure()
+x1 = [2, 4, 6, 8]
+y1 = [3, 5, 7, 9]
+plt.plot(x1, y1, '-.')
+ 
 plt.show()
