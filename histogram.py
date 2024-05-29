b481458334d51a2721bfba5c78ce8e91f39ea920
arshnoor1411 <arshnoor@infocusp.com>
2024-05-29 15:58:47 +0530

    enhance: custmoized histogram added

--- histogram.py
+++ histogram.py
@@ -1,8 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 
 x = np.random.normal(170, 10, 250)
 
 plt.hist(x)
 plt.show()
 print(x)
+
+# Customized Histogram
+data = np.random.randn(1000)
+ 
+sns.histplot(data, bins=30, kde=True, color='lightgreen', edgecolor='red')
+ 
+plt.xlabel('Values')
+plt.ylabel('Density')
+plt.title('Customized Histogram with Density Plot')
+ 
+plt.show()
